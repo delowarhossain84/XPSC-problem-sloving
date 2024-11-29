11afe660b515500ccb9956ba8e9298d8566fbc92
delowarhossain84 <165890225+delowarhossain84@users.noreply.github.com>
2024-11-29 23:56:25 +0600

    Add files via upload
    day 3

---
+++ week_5/day_1/day_3/OR_Permutation.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define nl "\n"
+#define ll long long int
+#define yes "YES"
+#define no "NO"
+#define sp  " " 
+#define fastread() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+using namespace std;
+int main(){
+   fastread()
+ int t;cin>>t;
+ while (t--)
+ {
+    int n;cin>>n;
+    for(int i=n;i>=1;i--) cout<<i<<sp;
+    cout<<nl;
+ }
+
+    return 0;
+}
