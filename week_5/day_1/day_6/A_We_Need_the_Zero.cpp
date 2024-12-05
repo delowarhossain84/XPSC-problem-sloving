f5ec773c1f38e61870df6223d760387de82c6cae
delowarhossain84 <165890225+delowarhossain84@users.noreply.github.com>
2024-12-05 23:28:48 +0600

    Add files via upload
    day 6

---
+++ week_5/day_1/day_6/A_We_Need_the_Zero.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define nl "\n"
+#define ll long long int
+#define yes "YES"
+#define no "NO"
+#define sp  " " 
+#define fastread() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ 
+using namespace std;
+int main(){
+   fastread()
+ int t;cin>>t;
+ while(t--){
+    int n;cin>>n;
+    vector<int>v(n),v2;
+    for(int i=0;i<n;i++) cin>>v[i];
+    bool f=false;
+        int Xor = 0;
+        for(int j=0;j<n;j++){
+            Xor^=v[j];
+    
+        }
+        
+
+        for(int i=0;i<n;i++) {
+            v2.push_back(Xor^v[i]);
+        }
+        
+        // for(int i=0;i<n;i++) cout<<v2[i]<<sp;
+       
+        int ans=0 ;
+        for(int k=0;k<n;k++){
+            ans ^=v2[k];
+         }
+
+        if(ans==0) cout<<Xor<<nl;
+        else cout<<-1<<nl;
+        
+        // cout<<Xor<<nl;
+ }
+    return 0;
+}
