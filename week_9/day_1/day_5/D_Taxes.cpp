d8ffc0217c3f05afc54ecc05c66270d61be5b45c
delowarhossain84 <165890225+delowarhossain84@users.noreply.github.com>
2024-12-28 23:50:40 +0600

    Add files via upload

---
+++ week_9/day_1/day_5/D_Taxes.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define nl "\n"
+#define ll long long int
+#define yes "YES"
+#define no "NO"
+#define sp  " " 
+#define fastread() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+
+bool is_prime(int x){
+    if(x==1) return false;
+    for(int i=2;i<x;i++){
+        if(x % i ==0) return false;
+           
+        }
+    return true;
+}
+
+void solve(){
+ int n;cin>>n;
+ if(is_prime(n)) cout<<1<<nl;
+ else if(is_prime(n-2) || n%2==0) cout<<2<<nl;
+ else cout<<3<<nl;
+ 
+    
+}
+int main(){
+   fastread()
+
+   solve();
+
+    return 0;
+}
