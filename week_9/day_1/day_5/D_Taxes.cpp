3fed30d6825c3aeaa709e7798a1ecad07dbd807b
delowarhossain84 <165890225+delowarhossain84@users.noreply.github.com>
2024-12-29 13:53:36 +0600

    Update D_Taxes.cpp

--- week_9/day_1/day_5/D_Taxes.cpp
+++ week_9/day_1/day_5/D_Taxes.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 bool is_prime(int x){
     if(x==1) return false;
-    for(int i=2;i<x;i++){
+    for(int i=2;i*i<=x;i++){
         if(x % i ==0) return false;
            
         }
